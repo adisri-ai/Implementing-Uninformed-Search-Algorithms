d443e54c45c2eed6a7a9f93ea1672a74aebfdbb2
Aditya Shrivastava <srivastavaaditya3@gmail.com>
2026-02-01 09:38:10 +0530

    Depth Limit Search

---
+++ depth_limit_search.py
@@ -0,0 +1,43 @@
+class Graph:
+    def __init__(self , vertices , directed , *edges):
+        self.n = len(vertices)
+        self.vertex_name ={}
+        idx = 0
+        for vertex in vertices:
+            self.vertex_name[idx] = vertex
+            idx+=1
+        self.directed = directed
+        self.adj = []
+        for i in range(self.n):
+            l = []
+            for j in range(self.n):
+                if((self.vertex_name[i],self.vertex_name[j]) in edges): l.append(1)
+                elif(self.directed == False and (self.vertex_name[j],self.vertex_name[i]) 
+                     in edges): 
+                    l.append(1)
+                else : l.append(0)
+            self.adj.append(l)
+    def dfs_traversal(self , root, target , curr , depth_limit):
+        if(curr>depth_limit): return False
+        if(self.vis[root]==1): return False
+        self.vis[root] = 1
+        self.dfs_order.append(root)
+        if(self.vertex_name[root] == target): return True
+        for j in range(self.n):
+            if(self.adj[root][j]==1 and self.vis[j] == False):
+                if(self.dfs_traversal(j , target , curr+1 , depth_limit)): return True
+        return False
+    def depth_limit_dfs(self , root = 0 , target = 0 , depth = 5):
+        self.dfs_order = []
+        self.vis = [0]*self.n
+        if(root not in self.vertex_name.values()): return []
+        root = [x for x  in self.vertex_name.keys() if self.vertex_name[x] == root][0]
+        k = self.dfs_traversal(root , target , 0, depth)
+        if(k==False): return []
+        return [self.vertex_name[x] for x in self.dfs_order]
+def main():        
+    g = Graph(['A' , 'B' , 'C' ,'D' , 'E' , 'F'] , False , ('A','B') , ('B','D') , ('B','E') , ('B','F'), ('A','C'))
+    ans = g.depth_limit_dfs(root = 'A' , target = 'F' , depth = 3) 
+    if(len(ans)==0): print("No path found")
+    else: print("Traversal order: " , ans , " depth: ")
+main()
